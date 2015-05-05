65fa4232783832633b08156495bee9731e6a5af5
mambax7 <mambax7@gmail.com>
2015-05-05 09:33:30 -0400

    Original files

---
+++ typeR/txc_themeinfo.php
@@ -0,0 +1,5 @@
+<?php 
+	$txc_themeinfo["name"] = "TypeR";
+	$txc_themeinfo["author"] = "mywebresource.com";
+	$txc_themeinfo["author_url"] = "http://mywebresource.com/modules/mydownloads/viewcat.php?cid=2";
+?>
